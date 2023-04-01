6da6a9581ea19febd8ee4c9fdcba2759587a93c3
A4-H20\niels <nielsbach@hey.com>
2023-04-01 08:05:45 +0200

    Implementing Cython stuff again

--- size_constrained_clustering/k_means_constrained/mincostflow_vectorized.py
+++ size_constrained_clustering/k_means_constrained/mincostflow_vectorized.py
@@ -4,9 +4,9 @@ import numpy as np
 from ortools.graph.python.min_cost_flow import SimpleMinCostFlow
 
 # Cython paths must be fully qualified
-from size_constrained_clustering.k_means_constrained.mincostflow_vectorized_ import \
-    SimpleMinCostFlow_AddArcWithCapacityAndUnitCostVectorized, SimpleMinCostFlow_SetNodeSupplyVectorized, \
-    SimpleMinCostFlow_FlowVectorized
+from k_means_constrained.mincostflow_vectorized_ import \
+    SimpleMinCostFlow_AddArcWithCapacityAndUnitCostVectorized, \
+    SimpleMinCostFlow_SetNodeSupplyVectorized, \
 
 
 class SimpleMinCostFlowVectorized(SimpleMinCostFlow):
