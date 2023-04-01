8fb19090c66df943c22bba1bbfb348f3686bc410
A4-H20\niels <nielsbach@hey.com>
2023-04-01 08:13:28 +0200

    Trying to change sources root

--- size_constrained_clustering/k_means_constrained/mincostflow_vectorized.py
+++ size_constrained_clustering/k_means_constrained/mincostflow_vectorized.py
@@ -4,14 +4,18 @@ import numpy as np
 from ortools.graph.python.min_cost_flow import SimpleMinCostFlow
 
 # Cython paths must be fully qualified
-from k_means_constrained.mincostflow_vectorized_ import \
-    SimpleMinCostFlow_AddArcWithCapacityAndUnitCostVectorized, \
-    SimpleMinCostFlow_SetNodeSupplyVectorized
+from k_means_constrained.mincostflow_vectorized_ import (
+    SimpleMinCostFlow_FlowVectorized,
+    SimpleMinCostFlow_SetNodeSupplyVectorized,
+    SimpleMinCostFlow_AddArcWithCapacityAndUnitCostVectorized,
+)
+
 
 class SimpleMinCostFlowVectorized(SimpleMinCostFlow):
-
     def AddArcWithCapacityAndUnitCostVectorized(self, tail, head, capacity, unit_cost):
-        return SimpleMinCostFlow_AddArcWithCapacityAndUnitCostVectorized(self, tail, head, capacity, unit_cost)
+        return SimpleMinCostFlow_AddArcWithCapacityAndUnitCostVectorized(
+            self, tail, head, capacity, unit_cost
+        )
 
     def SetNodeSupplyVectorized(self, node, supply):
         return SimpleMinCostFlow_SetNodeSupplyVectorized(self, node, supply)
